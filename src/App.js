90aff252ae17a312d543564846b4036de93447af
kyomin <kim031504@naver.com>
2021-12-13 12:44:51 +0900

    on/off 버튼 클릭 시 버튼 disabled 및 테스트

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 function App() {
 	const [counter, setCounter] = useState(0);
+	const [disabled, setDisabled] = useState(false);
 
 	return (
 		<div className='App'>
@@ -11,19 +12,22 @@ function App() {
 				<div>
 					<button
 						data-testid='minus-button'
-						onClick={() => setCounter((count) => count - 1)}>
+						onClick={() => setCounter((count) => count - 1)}
+						disabled={disabled}>
 						-
 					</button>
 					<button
 						data-testid='plus-button'
-						onClick={() => setCounter((count) => count + 1)}>
+						onClick={() => setCounter((count) => count + 1)}
+						disabled={disabled}>
 						+
 					</button>
 				</div>
 				<div>
 					<button
 						data-testid='on/off-button'
-						style={{ backgroundColor: 'blue' }}>
+						style={{ backgroundColor: 'blue' }}
+						onClick={() => setDisabled((prev) => !prev)}>
 						on/off
 					</button>
 				</div>
